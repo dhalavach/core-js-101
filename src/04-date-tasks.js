d38cedc080db39f9ee300b160c2c1b19cd50fd12
Dzmitry Halavach <kotik1@protonmail.com>
2022-11-28 02:22:47 +0300

    feat: date functions

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -73,8 +73,22 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+  const delta = end.getTime() - start.getTime();
+  const hours = String(Math.floor(delta / 3600000)).padStart(2, '0');
+  const minutes = String(Math.floor((delta % 3600000) / 60000)).padStart(
+    2,
+    '0',
+  );
+  const seconds = String(
+    Math.floor((delta - hours * 3600000 - minutes * 60000) / 1000),
+  ).padStart(2, '0');
+  const milliseconds = String(
+    delta - hours * 3600000 - minutes * 60000 - seconds * 1000,
+  ).padStart(3, '0');
+  return `${hours}:${minutes}:${seconds}.${milliseconds}`;
 }
 
 /**
