764d34015159bde31aeb0de1abe3c7d3df2a4653
Dzmitry Halavach <kotik1@protonmail.com>
2022-11-28 17:43:45 +0300

    feat: conditions and loops functions

--- src/scratch.js
+++ src/scratch.js
@@ -1,35 +1,9 @@
 /* eslint-disable */
-
-function angleBetweenClockHands(date) {
-  const dt = new Date(date);
-  const minutes = dt.getUTCMinutes();
-  const hours = dt.getUTCHours() % 12 || 12;
-  const minAngle = minutes * (360 / 60);
-  const hourAngle = hours * (360 / 12) + minutes / 2;
-  const degreesAngle = Math.abs(hourAngle - minAngle);
-  const smallerAngle = Math.min(360 - degreesAngle, degreesAngle);
-
-  return smallerAngle;
+function getFactorial(n) {
+  if (n === 1) {
+    return 1;
+  }
+  return n * getFactorial(n - 1);
 }
 
-// function angleBetweenClockHands(date) {
-//   const dt = new Date(date);
-
-//   const pi2 = Math.PI * 2;
-//   const m = dt.getUTCMinutes() / 60;
-//   const h = (dt.getUTCHours() + m) / 12;
-
-//   const angle = Math.abs(h - m) * pi2;
-//   return Math.min(angle, pi2 - angle);
-// }
-
-const d1 = Date.UTC(2016, 3, 5, 3, 0);
-const d2 = Date.UTC(2016, 3, 5, 15, 0);
-const d3 = Date.UTC(2016, 3, 5, 9, 0);
-const d4 = Date.UTC(2016, 3, 5, 21, 0);
-
-console.log(angleBetweenClockHands(d1));
-console.log(angleBetweenClockHands(d2));
-console.log(angleBetweenClockHands(d3));
-console.log(angleBetweenClockHands(d4));
-console.log(d2.toString());
+console.log(getFactorial(5));
