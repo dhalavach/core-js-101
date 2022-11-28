67e9be48fa85d3b1553b4e8be85615752f36bea1
Dzmitry Halavach <kotik1@protonmail.com>
2022-11-28 17:13:51 +0300

    feat: promises functions

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -5,7 +5,6 @@
  *                                                                                                *
  ************************************************************************************************ */
 
-
 /**
  * Return Promise object that is resolved with string value === 'Hooray!!! She said "Yes"!',
  * if boolean value === true is passed, resolved with string value === 'Oh no, she said "No".',
@@ -28,10 +27,15 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+function willYouMarryMe(isPositiveAnswer) {
+  return new Promise((resolve, reject) => {
+    if (typeof isPositiveAnswer !== 'boolean') {
+      reject(new Error('Wrong parameter is passed! Ask her again.'));
+    }
+    if (isPositiveAnswer) resolve('Hooray!!! She said "Yes"!');
+    resolve('Oh no, she said "No".');
+  });
 }
-
 
 /**
  * Return Promise object that should be resolved with array containing plain values.
@@ -48,8 +52,8 @@ function willYouMarryMe(/* isPositiveAnswer */) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+  return Promise.all(array);
 }
 
 /**
@@ -71,8 +75,8 @@ function processAllPromises(/* array */) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+  return Promise.race(array);
 }
 
 /**
@@ -92,8 +96,33 @@ function getFastestPromise(/* array */) {
  *    });
  *
  */
-function chainPromises(/* array, action */) {
-  throw new Error('Not implemented');
+function chainPromises(array, action) {
+  function promiseAllPolyfill(promises) {
+    const responses = [];
+    const errorResp = [];
+    return new Promise((resolve, reject) => {
+      promises.forEach(async (singlePromise, i) => {
+        try {
+          const res = await singlePromise;
+          responses.push(res);
+          if (i === promises.length - 1) {
+            if (errorResp.length > 0) {
+              reject(errorResp);
+            } else {
+              resolve(responses);
+            }
+          }
+        } catch (err) {
+          errorResp.push(err);
+          reject(err);
+        }
+      });
+    });
+  }
+  return promiseAllPolyfill(array).then(
+    (res) => res.reduce(action),
+    (err) => new Error(err),
+  );
 }
 
 module.exports = {
