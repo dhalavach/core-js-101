38221db6c70ae9adb8012e19a3e668705c5ece37
Dzmitry Halavach <kotik1@protonmail.com>
2022-12-14 12:16:45 +0300

    feat: function memoization

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -117,8 +117,15 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  const rt = (f, a) => {
+    let n = 0;
+    while (n < a) {
+      f();
+      n += 1;
+    }
+  };
+  return rt(func, attempts);
 }
 
 /**
--- src/scratch.js
+++ src/scratch.js
@@ -1,14 +1,9 @@
 /* eslint-disable */
 // const res = (x) => args[0] * x ** 2 + args[1] * x + args[2];
 
-function getPolynom(...args) {
-  const c1 = args[0];
-  const c2 = args[1] || 0;
-  const c3 = args[2] || 0;
-  if (args.length === 1) return (x) => args[0];
-  const res = (x) =>
-    c1 * x ** (args.length - 1) + c2 * x ** (args.length - 2) + c3;
-  return res;
+function makeTuple(first, second) {
+  return [first, second];
 }
 
-console.log(getPolynom(8)(0));
+const tuple = makeTuple(true, 'asdf');
+console.log(tuple);
